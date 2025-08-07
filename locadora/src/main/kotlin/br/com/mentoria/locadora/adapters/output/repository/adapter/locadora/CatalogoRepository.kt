86a7021960a4fb31ebd4260e279541ecfd22861b
JP <jobis_92@hotmail.com>
2025-08-07 19:03:43 -0300

    removendo projeto locadora

--- locadora/src/main/kotlin/br/com/mentoria/locadora/adapters/output/repository/adapter/locadora/CatalogoRepository.kt
+++
@@ -1,7 +0,0 @@
-package br.com.mentoria.locadora.adapters.output.repository.adapter.locadora
-
-import br.com.mentoria.locadora.adapters.output.repository.entity.LocadoraEntity
-import org.springframework.data.repository.CrudRepository
-
-interface CatalogoRepository : CrudRepository<LocadoraEntity, Long> {
-}
